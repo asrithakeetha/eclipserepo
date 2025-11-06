e3fe3387e96a3319b8551dc2b3fa03c46bfc6275
asrithakeetha <asrithakeetha@gmail.com>
2025-11-06 12:42:23 +0530

    initial commit

---
+++ JDBC1/src/com/connection/DemoClass.java
@@ -0,0 +1,17 @@
+package com.connection;
+
+/*1. import - java.sql
+* 2. load and reginster the driver - com.mysql.jdbc.Driver
+* 3. Create connection
+* 4. create a statement
+* 5. execute the query
+* 6. process the results
+* 7. close 
+*/
+public class DemoClass {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
